f811cfa3bf9b3200c43a8363cfa050b266797990
IngDavidTM <david-1011@hotmail.com>
2022-11-01 15:42:21 -0500

    add react router dom

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import react from 'react';
+
+const Navbar = () => {
+  return(
+    <nav className='navbar'>
+      <header>Bookstore CMS</header>
+      <ul></ul>
+    </nav>
+  );
+};
+
+export default Navbar;
